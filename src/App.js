bbc6f42ac10cf45cf9a806370ed0762c6eb9fd37
John Ho <ho.john2561@gmail.com>
2021-06-12 18:31:02 -0400

    Added Navbar without functionality. Finished rmeoving React boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
