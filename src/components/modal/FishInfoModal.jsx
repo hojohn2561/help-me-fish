54cc551885495c689404bd01ad850193b4dde324
John Ho <ho.john2561@gmail.com>
2021-07-22 12:39:16 -0400

    Refactor handling of inexistent fish data in info modal

--- src/components/modal/FishInfoModal.jsx
+++ src/components/modal/FishInfoModal.jsx
@@ -6,8 +6,17 @@ import "./fishInfoModal.scss";
 export default function FishInfoModal({ name, fishData, isVisible, onClose }) {
   console.log(fishData);
 
-  if (!fishData) return null;
+  // Fish data doesn't exist, just return blank modal
+  if (!fishData)
+    return (
+      <Modal isVisible={isVisible} onClose={onClose} height="75%" width="75%">
+        <div className="info-content">
+          <h1>{name}</h1>
+        </div>
+      </Modal>
+    );
 
+  // Fish data does exist, if generalInfo does too, display it on the modal, otherwise, return null
   return (
     <Modal isVisible={isVisible} onClose={onClose} height="75%" width="75%">
       <div className="info-content">
