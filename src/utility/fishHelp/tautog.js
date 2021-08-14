1c8040e86a69c32a282292499414b5156a44ba0a
John Ho <ho.john2561@gmail.com>
2021-08-14 18:22:26 -0400

    Tautog general/specific help, and bait, added.

--- src/utility/fishHelp/tautog.js
+++ src/utility/fishHelp/tautog.js
@@ -16,6 +16,31 @@ function getSpecificHelpIntro(
 ) {
   let helpStr = `Fishing for tautog when the water temperature is ${waterTemperature}°F `;
 
+  // Selected water temperature is inside the fish's ideal range
+  if (isIdealTemp) {
+    helpStr += `is a good idea. `;
+
+    // Selected cloud condition is one of the ideal cloud conditions for this fish
+    if (idealCloudConditions.includes(cloudCondition))
+      helpStr += `The sky being ${cloudCondition.toLowerCase()} is also good. `;
+    // Selected cloud condition is NOT one of the ideal cloud conditions for this fish
+    else
+      helpStr += `However, the weather condition being ${cloudCondition.toLowerCase()} is not good. `;
+  }
+  // Selected water temperature is NOT inside the fish's ideal range
+  else {
+    helpStr += `may be difficult. Ideally, you'd want the water temperature to be ${idealTemperatureRange}. `;
+
+    // Selected cloud condition is one of the ideal cloud conditions for this fish
+    if (idealCloudConditions.includes(cloudCondition))
+      helpStr += `However, the weather condition being ${cloudCondition.toLowerCase()} is good. `;
+    // Selected cloud condition is NOT one of the ideal cloud conditions for this fish
+    else
+      helpStr += `The weather condition being ${cloudCondition.toLowerCase()} is also not good. `;
+  }
+
+  helpStr += `Water clarity isn't that important when targeting tog.`;
+
   return helpStr;
 }
 
@@ -27,13 +52,19 @@ function getSpecificLures() {
     types: {},
   };
 
-  // Lure names
+  // Bait/Lure names
+  const { crabs } = constants.lures;
 
   // Strings describing the lures
+  const { general: crabsStr } = saltwaterFishLureStrings[tautog][crabs];
 
   let lureTypes = {};
 
   // Choosing which description to display, based on the user's input to the form
+  lureTypes[crabs] = {
+    message: crabsStr,
+    image: checkmark,
+  };
 
   luresInfo.types = lureTypes;
 
