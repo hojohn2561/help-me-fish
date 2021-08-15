844eb5d66f6362a1cef5683d7a0c17618dd731df
John Ho <ho.john2561@gmail.com>
2021-08-14 20:33:02 -0400

    Added general/specific help for fluke, and their lure/bait

--- src/utility/fishHelp/summerFlounder.js
+++ src/utility/fishHelp/summerFlounder.js
@@ -16,6 +16,31 @@ function getSpecificHelpIntro(
 ) {
   let helpStr = `Fishing for summer flounder when the water temperature is ${waterTemperature}°F `;
 
+  // Selected water temperature is inside the fish's ideal range
+  if (isIdealTemp) {
+    helpStr += `is a good idea. `;
+
+    // Selected cloud condition is one of the ideal cloud conditions for this fish
+    if (idealCloudConditions.includes(cloudCondition))
+      helpStr += `The sky being ${cloudCondition.toLowerCase()} is also good. `;
+    // Selected cloud condition is NOT one of the ideal cloud conditions for this fish
+    else
+      helpStr += `However, the weather condition being ${cloudCondition.toLowerCase()} is not good. `;
+  }
+  // Selected water temperature is NOT inside the fish's ideal range
+  else {
+    helpStr += `may be difficult. Ideally, you'd want the water temperature to be ${idealTemperatureRange}. `;
+
+    // Selected cloud condition is one of the ideal cloud conditions for this fish
+    if (idealCloudConditions.includes(cloudCondition))
+      helpStr += `However, the weather condition being ${cloudCondition.toLowerCase()} is good. `;
+    // Selected cloud condition is NOT one of the ideal cloud conditions for this fish
+    else
+      helpStr += `The weather condition being ${cloudCondition.toLowerCase()} is also not good. `;
+  }
+
+  helpStr += `Water clarity isn't that important when targeting fluke.`;
+
   return helpStr;
 }
 
@@ -27,13 +52,31 @@ function getSpecificLures() {
     types: {},
   };
 
-  // Lure names
+  // Bait/Lure names
+  const { gulp, bucktails, croakers } = constants.lures;
 
   // Strings describing the lures
+  const { general: gulpStr } = saltwaterFishLureStrings[summerFlounder][gulp];
+  const { general: bucktailsStr } =
+    saltwaterFishLureStrings[summerFlounder][bucktails];
+  const { general: croakersStr } =
+    saltwaterFishLureStrings[summerFlounder][croakers];
 
   let lureTypes = {};
 
   // Choosing which description to display, based on the user's input to the form
+  lureTypes[gulp] = {
+    message: gulpStr,
+    image: checkmark,
+  };
+  lureTypes[bucktails] = {
+    message: bucktailsStr,
+    image: checkmark,
+  };
+  lureTypes[croakers] = {
+    message: croakersStr,
+    image: checkmark,
+  };
 
   luresInfo.types = lureTypes;
 
