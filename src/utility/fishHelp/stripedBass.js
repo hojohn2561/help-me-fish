c27b111b58fa36f6cfd29cc1b8ae143d1dfe7cbd
John Ho <ho.john2561@gmail.com>
2021-08-15 01:03:15 -0400

    Added general/specific help for striper, and their lure/bait

--- src/utility/fishHelp/stripedBass.js
+++ src/utility/fishHelp/stripedBass.js
@@ -16,6 +16,31 @@ function getSpecificHelpIntro(
 ) {
   let helpStr = `Fishing for stripers when the water temperature is ${waterTemperature}°F `;
 
+  // Selected water temperature is inside the fish's ideal range
+  if (isIdealTemp) {
+    helpStr += `is a good idea. `;
+
+    // Selected cloud condition is one of the ideal cloud conditions for this fish
+    if (idealCloudConditions.includes(cloudCondition))
+      helpStr += `The sky being ${cloudCondition.toLowerCase()} is also good. `;
+    // Selected cloud condition is NOT one of the ideal cloud conditions for this fish
+    else
+      helpStr += `However, the weather condition being ${cloudCondition.toLowerCase()} is not good. `;
+  }
+  // Selected water temperature is NOT inside the fish's ideal range
+  else {
+    helpStr += `may be difficult. Ideally, you'd want the water temperature to be ${idealTemperatureRange}. `;
+
+    // Selected cloud condition is one of the ideal cloud conditions for this fish
+    if (idealCloudConditions.includes(cloudCondition))
+      helpStr += `However, the weather condition being ${cloudCondition.toLowerCase()} is good. `;
+    // Selected cloud condition is NOT one of the ideal cloud conditions for this fish
+    else
+      helpStr += `The weather condition being ${cloudCondition.toLowerCase()} is also not good. `;
+  }
+
+  helpStr += `Water clarity isn't that important when targeting stripers.`;
+
   return helpStr;
 }
 
@@ -28,12 +53,24 @@ function getSpecificLures() {
   };
 
   // Lure names
+  const { poppers, bunker } = constants.lures;
 
   // Strings describing the lures
+  const { general: poppersStr } =
+    saltwaterFishLureStrings[stripedBass][poppers];
+  const { general: bunkerStr } = saltwaterFishLureStrings[stripedBass][bunker];
 
   let lureTypes = {};
 
   // Choosing which description to display, based on the user's input to the form
+  lureTypes[poppers] = {
+    message: poppersStr,
+    image: checkmark,
+  };
+  lureTypes[bunker] = {
+    message: bunkerStr,
+    image: checkmark,
+  };
 
   luresInfo.types = lureTypes;
 
